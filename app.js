814db81a08e5a1057a8b6e5f0fdb9c84918dc132
May Barani <maybarani235@gmail.com>
2025-04-30 01:01:39 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello AWS from May!'));
+app.get('/', (req, res) => res.send('Hello Review Pipeline from May!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
